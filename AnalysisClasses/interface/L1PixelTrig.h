fe86807c8426189574a67d7c93744ef847dfe1b9
demattia <>
2008-01-21 16:13:16 +0000

    *** empty log message ***

--- AnalysisClasses/interface/L1PixelTrig.h
+++ AnalysisClasses/interface/L1PixelTrig.h
@@ -6,32 +6,49 @@
 template <class T1>
 class L1PixelTrig {
  public:
-  L1PixelTrig( int PJNUMBER, double DZ = 0, int NUMTKCUT = 0 ) {
-    pjnumber_ = PJNUMBER;
+  L1PixelTrig( int PJNUMBERCUT, double DZ = 0, int NUMTKCUT = 0, float PTCUT = 0. ) {
+    pjNumberCut_ = PJNUMBERCUT;
+    ptCut_ = PTCUT;
     dz_ = DZ;
     numTkCut_ = NUMTKCUT;
     response_ = false;
+    pjNumber_ = 0;
+    primVertexPt_ = 0.;
   }
 //  void Fill( vector<const PixelJet> );
   void Fill( const vector<T1> & vec_pj );
   bool Response() const {
     return response_;
   }
+  bool Response( int PJNUMBERCUT, float PTCUT = 0. ) {
+    if ( (pjNumber_ >= PJNUMBERCUT) && (primVertexPt_ > PTCUT) ) {
+      response_ = true;
+    }
+    else {
+      response_ = false;
+    }
+    return response_;
+  }
   auto_ptr<vector<Vertex<T1> > > VevVertex () {
     return vertex_vec_aptr;
   }
  private:
-  int pjnumber_;
+  int pjNumberCut_;
+  float ptCut_;
+  int pjNumber_;
   bool response_;
   vector<const T1 *> vec_pj_ptr;
   auto_ptr<vector<Vertex<T1> > > vertex_vec_aptr;
   double dz_;
   int numTkCut_;
+  float primVertexPt_;
 };
 
 template <class T1>
 //void L1PixelTrig::Fill ( vector<const PixelJet> & vec_pj ) {
 void L1PixelTrig<T1>::Fill ( const vector<T1> & vec_pj ) {
+
+  pjNumber_ = 0;
 
   if ( vec_pj.size() != 0 ) {
 
@@ -67,7 +84,9 @@ void L1PixelTrig<T1>::Fill ( const vector<T1> & vec_pj ) {
       //       cout << "trig number["<<c<<"] = " << it_t->number() << endl;
       //     }
 
-      if ( (vertex_vec_aptr->back()).number() >= pjnumber_ ) {
+      pjNumber_ = (vertex_vec_aptr->back()).number();
+      primVertexPt_ = (vertex_vec_aptr->back()).pt();
+      if ( (pjNumber_ >= pjNumberCut_) && (primVertexPt_ > ptCut_) ) {
         response_ = true;
       }
     }
