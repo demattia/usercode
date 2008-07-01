5c89608046d4a565cca74e566d8bc9a55ff355a6
demattia <>
2008-07-01 08:23:33 +0000

    *** empty log message ***

--- AnalysisClasses/interface/L1Trig.h
+++ AnalysisClasses/interface/L1Trig.h
@@ -1,19 +1,21 @@
 #ifndef L1TRIG_H
 #define L1TRIG_H
 //#define DEBUG
-////////////////////////////////////////////////////////////////////////////
-//
-// This class gives the L1 trigger response. It requires a vector of Jet
-// objects (Jet objects are included) and can also get the L1MET.
-// the L1 response is given by 
-//
-////////////////////////////////////////////////////////////////////////////
+/**
+ *
+ * This class gives the L1 trigger response. It requires a vector of Jet
+ * objects (Jet objects are included) and can also get the L1MET.
+ * the L1 response is given by 
+ *
+ */
 #include <vector>
 #include <algorithm>
 #include "SimpleJet.h"
+#include "AnalysisExamples/AnalysisObjects/interface/BaseJet.h"
 // used to pass calibrated jets to this class, see Init_L1Trig
 
 using namespace std;
+using namespace anaobj;
 
 class L1Trig {
   float _calib_jet1_et;
@@ -25,6 +27,10 @@ class L1Trig {
   bool et2_cut_;
   bool et3_cut_;
   bool et4_cut_;
+  bool response_;
+  bool responseNoFor_;
+  bool responseMEtJet_;
+  bool responseMEtJetNoFor_;
 
  public:
   L1Trig () {
@@ -37,19 +43,29 @@ class L1Trig {
     et2_cut_ = false;
     et3_cut_ = false;
     et4_cut_ = false;
+    response_ = false;
+    responseNoFor_ = false;
+    responseMEtJet_ = false;
+    responseMEtJetNoFor_ = false;
+
   }
 
-  // Function to sort the vector in Pt
+  /// Function to sort the vector in Pt
   void Sort (vector<SimpleJet> &);
-  // Requires a vector of Jet objects and a float for the MET
-  void Fill (vector<SimpleJet> &, float);
-  // alternate method requiring only the vector of jet, MET is set = 0
-  void Fill (vector<SimpleJet> & );
-  // Get the standard L1 response for the Multijet Trigger
+  /// Requires a vector of Jet objects and a float for the MEt
+  void Fill (vector<SimpleJet> &, float L1MEt = 0);
+  /// Requires three vectors of Jet objects and a bool default true asking if the forward jets must be used
+  void Fill (const BaseJetCollection &, const BaseJetCollection &, const BaseJetCollection &, const double & MEt = 0);
+  /** Get the response of the trigger. The bool is required: true = use L1FJets
+   * false = do not use L1FJets.
+   * Returns a pair(multijet response, MEt+Jet response).
+   */
+  pair<bool,bool> Response (bool useForwardL1Jets);
+  /// Get the standard L1 response for the Multijet Trigger
   bool Response ();
-  // Response to the L1 Multijet trigger for the assigned cuts
+  /// Response to the L1 Multijet trigger for the assigned cuts
   bool Response (float, float, float, float);
-  // Response to the L1 MET+Jet trigger for the assigned cuts (this order)
+  /// Response to the L1 MET+Jet trigger for the assigned cuts (this order)
   bool Response (float, float);
   bool Et1Cut () {
     return et1_cut_;
--- AnalysisClasses/src/L1Trig.cc
+++ AnalysisClasses/src/L1Trig.cc
@@ -35,28 +35,92 @@ void L1Trig::Fill (vector<SimpleJet> & vec_L1jet, float L1_met) {
   }
   _L1_met = L1_met;
 }
-void L1Trig::Fill (vector<SimpleJet> & vec_L1jet) {
-  // sort the vector in Pt
-  Sort(vec_L1jet);
 
-  _calib_jet1_et = 0;
-  _calib_jet2_et = 0;
-  _calib_jet3_et = 0;
-  _calib_jet4_et = 0;
-  _L1_met = 0;
+// Requires three vectors of Jet objects and a float for MEt
+void L1Trig::Fill (const BaseJetCollection & vec_L1CenJet, const BaseJetCollection & vec_L1ForJet, const BaseJetCollection & vec_L1TauJet, const double & L1MEt) {
 
-  if (vec_L1jet.size() > 0) {
-    _calib_jet1_et = vec_L1jet.at(0).pt();
+  response_ = false;
+  responseNoFor_ = false;
+  responseMEtJet_ = false;
+  responseMEtJetNoFor_ = false;
+
+  vector<SimpleJet> vec_TriggerCenJet;
+  vector<SimpleJet> vec_TriggerForJet;
+  vector<SimpleJet> vec_TriggerTauJet;
+  for ( BaseJetCollection::const_iterator tcj = vec_L1CenJet.begin(); tcj != vec_L1CenJet.end(); ++tcj ) {
+    vec_TriggerCenJet.push_back( SimpleJet( tcj->et(), tcj->eta(), tcj->phi() ) );
   }
-  if (vec_L1jet.size() > 1) {
-    _calib_jet2_et = vec_L1jet.at(1).pt();
+  for ( BaseJetCollection::const_iterator tfj = vec_L1ForJet.begin(); tfj != vec_L1ForJet.end(); ++tfj ) {
+    vec_TriggerForJet.push_back( SimpleJet( tfj->et(), tfj->eta(), tfj->phi() ) );
   }
-  if (vec_L1jet.size() > 2) {
-    _calib_jet3_et = vec_L1jet.at(2).pt();
+  for ( BaseJetCollection::const_iterator ttj = vec_L1TauJet.begin(); ttj != vec_L1TauJet.end(); ++ttj ) {
+    vec_TriggerTauJet.push_back( SimpleJet( ttj->et(), ttj->eta(), ttj->phi() ) );
   }
-  if (vec_L1jet.size() > 3) {
-    _calib_jet4_et = vec_L1jet.at(3).pt();
+
+  // Multijet
+  // --------
+
+  // Central
+  // -------
+  bool response_cen = false;
+  Fill( vec_TriggerCenJet );
+  response_cen = Response();  
+  // Forward
+  // -------
+  bool response_for = false;
+  Fill( vec_TriggerForJet );
+  response_for = Response();
+  // Tau
+  // ---
+  bool response_tau = false;
+  Fill( vec_TriggerTauJet );
+  response_tau = Response();
+
+  // Full and no-forward
+  // -------------------
+  response_ = ( response_cen || response_tau || response_for );
+  responseNoFor_ = ( response_cen || response_tau );
+
+  // MEt + Jet
+  // ---------
+  if ( L1MEt > 0. ) {
+    // Central
+    // -------
+    bool response_MEtJet_cen = false;
+    sort( vec_TriggerCenJet.rbegin(), vec_TriggerCenJet.rend() );
+    if ( vec_TriggerCenJet.size() != 0 ) {
+      if ( (vec_TriggerCenJet[0].pt() >= 80.) && (L1MEt >= 100.) ) {
+        response_MEtJet_cen = true;
+      }
+    }
+    // Tau
+    // ---
+    bool response_MEtJet_tau = false;
+    sort( vec_TriggerTauJet.rbegin(), vec_TriggerTauJet.rend() );
+    if ( vec_TriggerTauJet.size() != 0 ) {
+      if ( (vec_TriggerTauJet[0].pt() >= 80.) && (L1MEt >= 100.) ) {
+        response_MEtJet_tau = true;
+      }
+    }
+    // Forward
+    // -------
+    bool response_MEtJet_for = false;
+    sort( vec_TriggerForJet.rbegin(), vec_TriggerForJet.rend() );
+    if ( vec_TriggerForJet.size() != 0 ) {
+      if ( (vec_TriggerForJet[0].pt() >= 80.) && (L1MEt >= 100.) ) {
+        response_MEtJet_for = true;
+      }
+    }
+    // Full and no-forward
+    // -------------------
+    responseMEtJet_ = ( response_MEtJet_cen || response_MEtJet_tau || response_MEtJet_for );
+    responseMEtJetNoFor_ = ( response_MEtJet_cen || response_MEtJet_tau );
   }
+}
+
+pair<bool,bool> L1Trig::Response(bool useForwardL1Jets) {
+  if (useForwardL1Jets) return (make_pair(response_, responseMEtJet_));
+  else return (make_pair(responseNoFor_, responseMEtJetNoFor_));
 }
 
 bool L1Trig::Response () {
