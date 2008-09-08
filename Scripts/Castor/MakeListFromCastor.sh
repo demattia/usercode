110946bc2bf062fe0eb0b10fbe28b9d80d6a0cba
demattia <>
2008-09-08 07:13:17 +0000

    *** empty log message ***

--- Scripts/Castor/MakeListFromCastor.sh
+++ Scripts/Castor/MakeListFromCastor.sh
@@ -1,35 +1,21 @@
 #!/bin/sh
 
-DIR=/castor/cern.ch/user/d/demattia/FastSim/PUHL
-
 if [ "$1" == "" ]; then
-    echo Please, select the sample,
-    echo chose from:
-    echo QCD_30-50
-    echo QCD_50-80
-    echo QCD_80-120
-    echo QCD_120-170
-    echo QCD_170-230
-    echo QCD_230-300
-    echo QCD_300-380
-    echo QCD_380-incl
-    echo TTH_120
-    echo EXTRA_QCD_30-50
-    echo ...
+    echo Please, input castor directory containing the root files
 else
 
-    rfdir ${DIR}/$1/ | awk '{print $9}'> temp_filelist.txt
+    rfdir $1 | awk '{print $9}'> temp_filelist.txt
 
-    if [ -e $1_castor.cff ]; then
-        rm $1_castor.cff
+    if [ -e castor.cff ]; then
+        rm castor.cff
     fi
 
     # Use wc (word count) to evaluate the number of lines
     # -l counts the newlines
     totlines=`cat temp_filelist.txt | wc -l`
 
-    echo "source = PoolSource {" >> $1_castor.cff
-    echo "  untracked vstring fileNames = {" >> $1_castor.cff
+    echo "source = PoolSource {" >> castor.cff
+    echo "  untracked vstring fileNames = {" >> castor.cff
 
     i=1
 
@@ -37,16 +23,16 @@ else
         if [ "$i" != "$totlines" ]; then
 #            echo line = $line
 #            echo $line | awk '{print $8}'
-            echo "    \""castor:${DIR}/$1/$line"\"," >> $1_castor.cff
+            echo "    \""castor:$1/$line"\"," >> castor.cff
         else
             # Do not put the "," at the end of the last file
-            echo "    \""castor:${DIR}/$1/$line"\"" >> $1_castor.cff
+            echo "    \""castor:$1/$line"\"" >> castor.cff
         fi
         i=$[$i+1]
     done
 
-    echo "  }" >> $1_castor.cff
-    echo "}" >> $1_castor.cff
+    echo "  }" >> castor.cff
+    echo "}" >> castor.cff
 
     rm temp_filelist.txt
 
