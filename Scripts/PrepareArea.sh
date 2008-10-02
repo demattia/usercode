83ad164ecaea54f64db309beb37c4c4092f0d953
demattia <>
2008-10-02 12:18:54 +0000

    *** empty log message ***

--- Scripts/PrepareArea.sh
+++ Scripts/PrepareArea.sh
@@ -28,9 +28,21 @@ mv UserCode/DeMattia/ttHMEtplusJetsAnalyzer CMSSW_1_8_4/src/AnalysisExamples
 # Remove UserCode dir
 rm -r UserCode
 
-# Comment the TDAna as it is not working now
+# Comment some files that are not up to date
 mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/TDAna.cc CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/TDAna.cc_backup
 mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/TDAna.h CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/TDAna.h_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/TDAnaProducer.cc CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/TDAnaProducer.cc_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/TDAnaProducer.h CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/TDAnaProducer.h_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/AlgValidator.cc CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/AlgValidator.cc_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/AlgValidator.h CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/AlgValidator.h_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/MultiplicationFilter.cc CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/MultiplicationFilter.cc_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/MultiplicationFilter.h CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/MultiplicationFilter.h_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/PTag.cc CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/PTag.cc_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/PTag.h CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/PTag.h_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/RecoAssociator.cc CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/RecoAssociator.cc_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/RecoAssociator.h CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/RecoAssociator.h_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/VertexAssoc.cc CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/src/VertexAssoc.cc_backup
+mv CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/VertexAssoc.h CMSSW_1_8_4/src/AnalysisExamples/L1PixelAnalyzer/interface/VertexAssoc.h_backup
 
 # Not sure this works, better to do it outside
 #echo Compiling the area
