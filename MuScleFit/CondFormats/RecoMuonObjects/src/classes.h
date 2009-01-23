752da795472f6007e6f34cc74a40d91d8a83d441
demattia <>
2009-01-23 17:43:36 +0000

    *** empty log message ***

--- MuScleFit/CondFormats/RecoMuonObjects/src/classes.h
+++ MuScleFit/CondFormats/RecoMuonObjects/src/classes.h
@@ -1,11 +1,9 @@
 #include "CondFormats/PhysicsToolsObjects/interface/Histogram2D.h"
-#include "CondFormats/RecoMuonObjects/interface/MuScleFitLikelihoodPdf.h"
 #include "CondFormats/RecoMuonObjects/interface/MuScleFitDBobject.h"
 
 namespace {
   // std::vector<PhysicsTools::Calibration::HistogramD2D> a;
   // std::vector<std::string> b;
   // std::vector<int> c;
-  MuScleFitLikelihoodPdf d;
   MuScleFitDBobject e;
 }
