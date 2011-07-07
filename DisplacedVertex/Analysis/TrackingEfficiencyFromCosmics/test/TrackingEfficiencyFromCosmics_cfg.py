65db5d8f441599a8882fea95f24f29808f2daf0a
demattia <>
2011-07-07 10:24:54 +0000

    Added new paramaters

--- DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/test/TrackingEfficiencyFromCosmics_cfg.py
+++ DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/test/TrackingEfficiencyFromCosmics_cfg.py
@@ -2,37 +2,50 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TrackingEfficiencyFromCosmics")
 
-# process.load("FWCore.MessageService.MessageLogger_cfi")
-# initialize MessageLogger and output report
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.threshold = 'INFO'
-process.MessageLogger.categories.append('Demo')
-process.MessageLogger.cerr.INFO = cms.untracked.PSet(
-    limit = cms.untracked.int32(-1)
+# # initialize MessageLogger and output report
+# process.load("FWCore.MessageLogger.MessageLogger_cfi")
+# # process.MessageLogger.cerr.threshold = 'INFO'
+# # process.MessageLogger.categories.append('Demo')
+# # process.MessageLogger.cerr.INFO = cms.untracked.PSet(
+# #     limit = cms.untracked.int32(-1)
+# # )
+
+process.load("FWCore.MessageService.test.Services_cff")
+# Here is the configuration of the MessgeLogger Service:
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring('Message'),
+    Message = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    )
 )
+
+
+
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        # 'file:/afs/cern.ch/user/d/demattia/scratch0/DisplacedVertex/CMSSW_4_2_2/src/reco_RAW2DIGI_L1Reco_RECO_DQM.root'
+        # 'file:hitRemoval_0.root'
+        # 'file:hitRemoval.root'
+        # 'file:/afs/cern.ch/user/d/demattia/scratch0/DisplacedVertex/CMSSW_4_2_2/src/reco_RAW2DIGI_L1Reco_RECO_DQM.root'
+    'file:/home/demattia/Simulation/WithGENInfo/reco_RAW2DIGI_L1Reco_RECOSIM_DQM.root'
+    )
+)
 
 process.TFileService=cms.Service('TFileService',
                                  fileName=cms.string('TrackingEfficiencyFromCosmics.root')
                                  )
 
-process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'file:/afs/cern.ch/user/d/demattia/scratch0/DisplacedVertex/CMSSW_4_2_2/src/reco_RAW2DIGI_L1Reco_RECO_DQM.root'
-        # 'file:hitRemoval_0.root'
-        # 'file:hitRemoval.root'
-    )
-)
-
 process.demo = cms.EDAnalyzer('TrackingEfficiencyFromCosmics',
-                              MaxDeltaR = cms.double(1000),
+                              MaxDeltaR = cms.double(1),
                               SimMaxDeltaR = cms.double(1000),
-                              UseMCtruth = cms.bool(False),
+                              UseMCtruth = cms.bool(True),
                               EffOutputFileName = cms.string("Efficiency.root")
-)
+                              )
 
 
 process.p = cms.Path(process.demo)
