cdd9fb4ee10471a23edd6c99f1f7b06193b3f652
demattia <>
2011-07-11 18:10:22 +0000

    Fixed bins

--- DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/interface/Utils.h
+++ DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/interface/Utils.h
@@ -8,12 +8,12 @@ namespace utils
 {
   TH1F * bookHistogram(edm::Service<TFileService> & fileService, const TString & name,
 		       const TString & nameForTitle, const TString axisTitle, const TString & unit,
-		       const int bins, const double min, const double max)
+		       const int bins, const double & min, const double & max)
   {
     TString underscore("");
     if( nameForTitle != "" ) underscore = "_";
 
-    TH1F * histo = fileService->make<TH1F>(name+underscore+nameForTitle, name+" "+nameForTitle,500,0,500);
+    TH1F * histo = fileService->make<TH1F>(name+underscore+nameForTitle, name+" "+nameForTitle, bins, min, max);
     histo->GetXaxis()->SetTitle(axisTitle+" "+unit);
     std::stringstream ss;
     ss << histo->GetBinWidth(1);
