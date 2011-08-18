bd6fdf38f41d7d6e634d07658d086fc0cee02cad
demattia <>
2011-08-18 09:13:28 +0000

    fixed variable name

--- DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/test/EfficiencyMerger_cfg.py
+++ DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/test/EfficiencyMerger_cfg.py
@@ -6,7 +6,7 @@ process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.demo = cms.EDAnalyzer('EfficiencyMerger',
-                              InputFileName = cms.vstring("EfficiencyCleaned_1.root", "EfficiencyCleaned_2.root")
+                              InputFileNames = cms.vstring("EfficiencyCleaned_1.root", "EfficiencyCleaned_2.root")
                               )
 
 process.p = cms.Path(process.demo)
