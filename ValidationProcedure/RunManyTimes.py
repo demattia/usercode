76e61ee4319e157cf535d0385f056b1d8f70d8e5
demattia <>
2011-09-16 13:29:51 +0000

    Updated to get parameters and peform cleanup

--- ValidationProcedure/RunManyTimes.py
+++ ValidationProcedure/RunManyTimes.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
+import sys
 import os
 import math
+
+if len(sys.argv) != 3:
+    print "Usage: RunManyTimes CFG N"
+    print "where CFG is the cfg to be run and N is the number of jobs to run"
+    sys.exit()
 
 vSizes = []
 vSizeAverage = 0
 
-numTrials = 50
+cfg = sys.argv[1]
+numTrials = int(sys.argv[2])
 
 for i in range(0, numTrials):
-    # print "cmsRun SiStripQualityStatistics_cfg.py > output.log"
-    # os.system("cmsRun SiStripQualityStatistics_cfg.py > output.log")
-    os.system("cmsRun SimpleCheck.py > output.log")
+    os.system("cmsRun "+cfg+" > output.log")
     file = open("output.log")
     for line in file:
         if line.find("MemoryReport") != -1:
@@ -19,9 +24,7 @@ for i in range(0, numTrials):
             vSizes.append(float(line.split()[4]))
             vSizeAverage += vSizes[i]
 
-# print vSizes
 vSizeAverage /= numTrials
-# print vSizeAverage
 
 rms = 0
 for value in vSizes:
@@ -29,3 +32,5 @@ for value in vSizes:
     rms = math.sqrt(rms)/numTrials
 
 print "mean memory used +/- rms =", vSizeAverage, "+/-", rms
+
+os.system("rm output.log")
