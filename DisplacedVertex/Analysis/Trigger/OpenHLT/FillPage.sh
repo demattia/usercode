94f83b69fe319d351167a2bdf3f88676db94914c
demattia <>
2011-09-27 14:42:54 +0000

    Changes to the html

--- DisplacedVertex/Analysis/Trigger/OpenHLT/FillPage.sh
+++ DisplacedVertex/Analysis/Trigger/OpenHLT/FillPage.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+mkdir -p WebPage
+mv *.png WebPage
+
 echo "Producing the plots"
 root -l -b -q drawCanvases.C
 
 echo "Appending the entries on the web page"
-cat template_page.html entriesFile.txt > page.html
-echo "</body></html>" >> page.html
+cat template_page.html entriesFile.txt > WebPage/page.html
+echo "</body></html>" >> WebPage/page.html
 
 echo "Copy this to the web browser to see the page:"
-echo `pwd`"/page.html"
+echo `pwd`"/WebPage/page.html"
