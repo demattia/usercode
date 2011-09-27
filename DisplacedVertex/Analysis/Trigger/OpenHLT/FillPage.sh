f79d42817130594d10ba7c094adbedd46619eedc
demattia <>
2011-09-27 15:07:33 +0000

    Fix

--- DisplacedVertex/Analysis/Trigger/OpenHLT/FillPage.sh
+++ DisplacedVertex/Analysis/Trigger/OpenHLT/FillPage.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 mkdir -p WebPage
-mv *.png WebPage
 
 echo "Producing the plots"
 root -l -b -q drawCanvases.C
+
+mv *.png WebPage
 
 echo "Appending the entries on the web page"
 cat template_page.html entriesFile.txt > WebPage/page.html
