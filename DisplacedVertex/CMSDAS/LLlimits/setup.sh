f71d1d76169057c46c458de77fdbfbffff7ae407
demattia <>
2012-01-09 20:32:34 +0000

    modified

--- DisplacedVertex/CMSDAS/LLlimits/setup.sh
+++ DisplacedVertex/CMSDAS/LLlimits/setup.sh
@@ -3,4 +3,4 @@
 . /afs/cern.ch/sw/lcg/app/releases/ROOT/5.30.00/x86_64-slc5-gcc43-opt/root/bin/thisroot.sh
 export PATH=/afs/cern.ch/sw/lcg/external/Python/2.6.5/x86_64-slc5-gcc43-opt/bin:$PATH
 export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Python/2.6.5/x86_64-slc5-gcc43-opt/lib:$LD_LIBRARY_PATH
-cd /afs/cern.ch/user/p/plujan/public/LLlimits
+cd /uscms_data/d3/demattia/CMSDAS/CMSSW_4_2_7/src/HarderAnalysis/DisplacedDileptons/test/LLlimits
