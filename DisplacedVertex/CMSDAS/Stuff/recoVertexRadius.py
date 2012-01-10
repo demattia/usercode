ae7430cfe03742c4284ec28b06f81861222c5fc9
yzheng <>
2012-01-10 04:32:55 +0000

    *** empty log message ***

--- DisplacedVertex/CMSDAS/Stuff/recoVertexRadius.py
+++ DisplacedVertex/CMSDAS/Stuff/recoVertexRadius.py
@@ -7,7 +7,7 @@ from DataFormats.FWLite import Events, Handle
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_200_050F/pat/PATtuple_1_1_ZcD.root')
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7//Signal_400_005L/pat/PATtuple_6_1_i1B.root')
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_400_020F/pat/PATtuple_4_1_BWV.root')
-events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_120_020F//pat/PATtuple_5_1_nlV.root')
+#events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_120_020F//pat/PATtuple_5_1_nlV.root')
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_1000_150F//pat/PATtuple_1_2_2Fr.root')
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_1000_020F//pat/PATtuple_9_1_bRA.root')
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_1000_050F/pat/PATtuple_10_1_HXU.root')
@@ -15,10 +15,11 @@ events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/C
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_1000_350F/pat/PATtuple_3_1_9mQ.root')
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_200_020F/pat/PATtuple_2_1_T2M.root')
 #events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_400_150F/pat/PATtuple_5_1_uR7.root')
-#events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_400_050F/pat/PATtuple_9_1_nd6.root')
-#events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_200_050F/pat/PATtuple_2_1_lE8.root')
+events = Events ('root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/CMSSW_4_2_7/Signal_400_050F/pat/PATtuple_9_1_nd6.root')
 handleMuon  = Handle ('std::vector<pat::Muon>')
+handle  = Handle ('std::vector<reco::GenParticle>')
 labelMuon = ("selectedPatMuons")
+label = ("genParticles")
 
 # a label is just a tuple of strings that is initialized just like an edm::InputTag
 #labelTrack = ("generalTracks")
@@ -39,6 +40,10 @@ transIPMuonHist = ROOT.TH1F ("vertexRadiusMuonHist", "Transverse Impact Paramete
 transIPMuonHist.GetXaxis().SetTitle("Transverse IP(cm)");
 ptMuHist = ROOT.TH1F ("ptMuHist", "ptMuHist", 500, 0, 500)
 ptMuHist.GetXaxis().SetTitle("p_{T}(GeV/c)")
+ptGenMuHist = ROOT.TH1F ("ptGenMuHist", "ptGenMuHist", 500, 0, 500)
+ptGenMuHist.GetXaxis().SetTitle("p_{T}(GeV/c)")
+transIPGenMuHist = ROOT.TH1F ("transIPGenMuHist", "transIPGenMuHist", 500, 0, 200)
+transIPGenMuHist.GetXaxis().SetTitle("Transverse IP(cm)");
 
 # loop over events
 for event in events:
@@ -52,25 +57,38 @@ for event in events:
 #    standAloneMuons = handleStandAlone.product()
     muons = handleMuon.product()
 
+    event.getByLabel(label, handle)
+
+    # get the product
+    genParticles = handle.product()
+
+    for genParticle in genParticles:
+	if abs(genParticle.pdgId()) == 13:
+		ptGenMuHist.Fill(genParticle.pt())
+		transIPGenMuHist.Fill( math.sqrt(genParticle.vx()**2 + genParticle.vy()**2) )
 #    for track in tracks:
 #        vertexRadiusTrackHist.Fill( math.sqrt(track.vx()**2 + track.vy()**2) )
 #    for standAloneMuon in standAloneMuons:
 #        vertexRadiusStandAloneHist.Fill( math.sqrt(standAloneMuon.vx()**2 + standAloneMuon.vy()**2) )
     for muon in muons:
-        transIPMuonHist.Fill( math.sqrt(muon.vx()**2 + muon.vy()**2) )
-        ptMuHist.Fill( muon.pt())
+	if muon.isGlobalMuon()==1:
+        	transIPMuonHist.Fill( math.sqrt(muon.vx()**2 + muon.vy()**2) )
+        	ptMuHist.Fill( muon.pt())
 # make a canvas, draw, and save it
-#c1 = ROOT.TCanvas()
-#vertexRadiusTrackHist.Draw()
-#c1.Print ("vertexRadiusTrack.png")
+c1 = ROOT.TCanvas()
+ptGenMuHist.Draw()
+c1.Print ("GenPtMuon_Signal_400_050F.png")
+ 
+c2 = ROOT.TCanvas()
+transIPGenMuHist.Draw()
+c2.Print ("transIPGenMu_Signal_400_050F.png")
 
-#c2 = ROOT.TCanvas()
-#vertexRadiusStandAloneHist.Draw()
-#c2.Print ("vertexRadiusStandAlone.png")
-#c1 = ROOT.TCanvas()
-#ptMuHist.Draw()
-#c1.Print ("ptMuHist.png")
 c3 = ROOT.TCanvas()
 transIPMuonHist.Draw()
-c3.Print ("transIPMuon_Signal_120_020F.png")
+c3.Print ("transIPMuon_Signal_400_050F_glb.png")
+#c3.SetLogy()
+c4 = ROOT.TCanvas()
+ptMuHist.Draw()
+#c4.SetLogy()
+c4.Print ("PtMuon_Signal_400_050F_glb.png")
 
