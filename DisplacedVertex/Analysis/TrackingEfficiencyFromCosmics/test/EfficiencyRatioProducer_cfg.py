255139ed785af45778182c551aa74e2fb924e036
zhenhu <>
2012-01-11 22:58:38 +0000

    change input names

--- DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/test/EfficiencyRatioProducer_cfg.py
+++ DisplacedVertex/Analysis/TrackingEfficiencyFromCosmics/test/EfficiencyRatioProducer_cfg.py
@@ -6,8 +6,9 @@ process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.demo = cms.EDAnalyzer('EfficiencyRatioProducer',
-                              InputFileNameNumerator = cms.string("Efficiency_Data.root"),
-                              InputFileNameDenominator = cms.string("Efficiency_Sim.root")
+                              InputFileNameNumerator = cms.string("efficiency_data/EfficiencyCleaned.root"),
+                              InputFileNameDenominator = cms.string("efficiency_sim/EfficiencyCleaned.root"),
+							  Rebin = cms.uint32(4)
                               )
 
 process.p = cms.Path(process.demo)
