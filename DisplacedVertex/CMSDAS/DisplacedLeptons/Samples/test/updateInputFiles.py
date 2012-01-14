3039806e90348ebd087e45bc8b59f5bd7b9aaebf
demattia <>
2012-01-14 07:40:30 +0000

    fix

--- DisplacedVertex/CMSDAS/DisplacedLeptons/Samples/test/updateInputFiles.py
+++ DisplacedVertex/CMSDAS/DisplacedLeptons/Samples/test/updateInputFiles.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 
 def file_len(fname):
@@ -12,42 +14,43 @@ samplesDir = "../python/samples/"
 sampleFilesList = os.listdir(samplesDir)
 # print sampleFilesList
 for fname in dirList:
-    if fname.startswith("filelist"):
-        startReplacing = False
+    # Skip empty filelist files
+    if fname.startswith("filelist") and os.path.getsize(os.getcwd()+"/"+fname) > 0:
+        replacing = False
         sampleName = fname.split(".")[1].split("_pat_")[0]
-        print sampleName
+        print "Updating file list for sample "+sampleName
         cffFileName = sampleName+"_cff.py"
         if cffFileName in sampleFilesList:
-            print "found"
             inputCff = open(samplesDir+cffFileName)
-            sampleCffName = sampleName+"_cff_new.py"
+            sampleCffName = samplesDir+sampleName+"_cff_new.py"
             outputCff = open(sampleCffName, "w")
             # Loop over the original cff
             for line in inputCff:
-                if startReplacing:
-                    break
-                if line.startswith("sampleBaseDir"):
-                    outputCff.write("sampleBaseDir = \"root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/\"+sampleProcessRelease+\"/Data_Mu_Run2011A1\"")
+                if replacing:
+                    if line.find("]") != -1:
+                        replacing = False
+                    pass
+                elif line.startswith("sampleBaseDir"):
+                    outputCff.write("sampleBaseDir = \"root://xrootd.rcac.purdue.edu//store/user/demattia/longlived/\"+sampleProcessRelease+\"/"+sampleName+"\"\n")
                 elif line.find("harder/longlived/") > 0:
                     pass
-                else:
-                    outputCff.write(line)
                 # stop at the samplePatFiles list
-                if line.startswith("samplePatFiles"):
-                    startReplacing = True
-            # Fill the rest with the new list of files
-            fileListFile = open(fname)
-            numFiles = file_len(fname)
-            counter = 1
-            # print "num = "+str(numFiles)
-            for line in fileListFile:
-                counter = counter + 1
-                if counter == numFiles:
-                    # print "replacing"
-                    outputCff.write(line.replace(",", "\n]"))
                 else:
                     outputCff.write(line)
+                    if line.startswith("samplePatFiles"):
+                        print "replacing"
+                        replacing = True
+                        # Fill the rest with the new list of files
+                        fileListFile = open(fname)
+                        numFiles = file_len(fname)
+                        counter = 1
+                        for line in fileListFile:
+                            counter = counter + 1
+                            if counter == numFiles:
+                                outputCff.write(line.replace(",", "\n]"))
+                            else:
+                                outputCff.write(line)
+
             outputCff.close()
-            # print "counter = "+str(counter) 
-            os.system("mv "+samplesDir+sampleCffName+" "+samplesDir+cffFileName)
+            os.system("mv "+sampleCffName+" "+samplesDir+cffFileName)
 
