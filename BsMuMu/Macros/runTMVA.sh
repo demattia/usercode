f9db92f847497030acc5f3d1ba1afc5c4b595730
demattia <>
2013-03-26 21:42:03 +0000

    Updates

---
+++ BsMuMu/Macros/runTMVA.sh
@@ -0,0 +1,10 @@
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"\"\) > TMVALog_barrel.txt &
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"\"\) > TMVALog_endcaps.txt &
+
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"0\"\) > TMVALog_barrel_0.txt &
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"1\"\) > TMVALog_barrel_1.txt &
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"2\"\) > TMVALog_barrel_2.txt &
+
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"0\"\) > TMVALog_endcaps_0.txt &
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"1\"\) > TMVALog_endcaps_1.txt &
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"2\"\) > TMVALog_endcaps_2.txt &
