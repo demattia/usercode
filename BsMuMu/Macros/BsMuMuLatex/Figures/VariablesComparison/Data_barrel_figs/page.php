136da674679ee8505e783284839e114a5e0f1e61
zhenhu <>
2013-03-29 04:45:12 +0000

    VariablesComparison

---
+++ BsMuMu/Macros/BsMuMuLatex/Figures/VariablesComparison/Data_barrel_figs/page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+
+<head>
+  <title>Data Barrel</title>
+</head>
+
+<body>
+  <h3>Data Barrel</h3>
+  <?
+     $handle = opendir(dirname(realpath(__FILE__)).'');
+     while($file = readdir($handle)) {
+     if($file !== '.' && $file !== '..') {
+     echo '<img src="'.$file.'" border="0" />';
+     }
+     }
+  ?>
+</body>
+
+</html>
+
