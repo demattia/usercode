3fbcebf11cdf897f86cde50d58c8303459dfdbc2
zhenhu <>
2013-04-11 17:21:57 +0000

    change figure directory

--- BsMuMu/Macros/Common/RunAllComp.sh
+++ BsMuMu/Macros/Common/RunAllComp.sh
@@ -9,6 +9,7 @@ rm -rf Data_barrel_figs_OLD
 mv Data_barrel_figs Data_barrel_figs_OLD
 root -l -b -q comp.C++\(\"/uscms/home/zhenhu/lpcmuon/BsMuMu/data/HackedTrees/Barrel_preselection.root\",\"data_main_barrel.root\"\)
 mv figs Data_barrel_figs
+cp Data_barrel_figs/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/Data_barrel_figs/
 cat page_template.php | sed s/"TITLE"/"Data Barrel"/g > Data_barrel_figs/page.php
 
 mkdir figs
@@ -16,6 +17,7 @@ rm -rf Data_endcaps_figs_OLD
 mv Data_endcaps_figs Data_endcaps_figs_OLD
 root -l -b -q comp.C++\(\"/uscms/home/zhenhu/lpcmuon/BsMuMu/data/HackedTrees/Endcaps_preselection.root\",\"data_main_endcaps.root\"\)
 mv figs Data_endcaps_figs
+cp Data_endcaps_figs/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/Data_endcaps_figs/
 cat page_template.php | sed s/"TITLE"/"Data Endcaps"/g > Data_endcaps_figs/page.php
 
 # MC
@@ -25,6 +27,7 @@ rm -rf MC_barrel_figs_OLD
 mv MC_barrel_figs MC_barrel_figs_OLD
 root -l -b -q comp.C++\(\"/uscms/home/zhenhu/lpcmuon/BsMuMu/data/HackedTrees/BsMC12_barrel_preselection.root\",\"MC_main_barrel.root\"\)
 mv figs MC_barrel_figs
+cp MC_barrel_figs/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/MC_barrel_figs/
 cat page_template.php | sed s/"TITLE"/"MC Barrel"/g > MC_barrel_figs/page.php
 
 mkdir figs
@@ -32,4 +35,5 @@ rm -rf MC_endcaps_figs_OLD
 mv MC_endcaps_figs MC_endcaps_figs_OLD
 root -l -b -q comp.C++\(\"/uscms/home/zhenhu/lpcmuon/BsMuMu/data/HackedTrees/BsMC12_endcaps_preselection.root\",\"MC_main_endcaps.root\"\)
 mv figs MC_endcaps_figs
+cp MC_endcaps_figs/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/MC_endcaps_figs/
 cat page_template.php | sed s/"TITLE"/"MC Endcaps"/g > MC_endcaps_figs/page.php
--- BsMuMu/Macros/Common/RunAllComp_3h.sh
+++ BsMuMu/Macros/Common/RunAllComp_3h.sh
@@ -10,6 +10,7 @@ rm -rf Data_barrel_figs_3h_OLD
 mv Data_barrel_figs_3h Data_barrel_figs_3h_OLD
 root -l -b -q comp_3h.C++\(\"Barrel_preselection_0.root\",\"Barrel_preselection_1.root\",\"Barrel_preselection_2.root\"\)
 mv figs_3h Data_barrel_figs_3h
+cp Data_barrel_figs_3h/*.pdf Data_barrel_figs_3h/*/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/Data_barrel_figs_3h/
 cat page_template.php | sed s/"TITLE"/"Data Barrel"/g > Data_barrel_figs_3h/page.php
 
 mkdir figs_3h
@@ -18,6 +19,7 @@ rm -rf Data_endcaps_figs_3h_OLD
 mv Data_endcaps_figs_3h Data_endcaps_figs_3h_OLD
 root -l -b -q comp_3h.C++\(\"Endcaps_preselection_0.root\",\"Endcaps_preselection_1.root\",\"Endcaps_preselection_2.root\"\)
 mv figs_3h Data_endcaps_figs_3h
+cp Data_endcaps_figs_3h/*.pdf Data_endcaps_figs_3h/*/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/Data_endcaps_figs_3h/
 cat page_template.php | sed s/"TITLE"/"Data Endcaps"/g > Data_endcaps_figs_3h/page.php
 
 # MC
@@ -28,6 +30,7 @@ rm -rf MC_barrel_figs_3h_OLD
 mv MC_barrel_figs_3h MC_barrel_figs_3h_OLD
 root -l -b -q comp_3h.C++\(\"BsMC12_barrel_preselection_0.root\",\"BsMC12_barrel_preselection_1.root\",\"BsMC12_barrel_preselection_2.root\"\)
 mv figs_3h MC_barrel_figs_3h
+cp MC_barrel_figs_3h/*.pdf MC_barrel_figs_3h/*/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/MC_barrel_figs_3h/
 cat page_template.php | sed s/"TITLE"/"MC Barrel"/g > MC_barrel_figs_3h/page.php
 
 mkdir figs_3h
@@ -36,4 +39,5 @@ rm -rf MC_endcaps_figs_3h_OLD
 mv MC_endcaps_figs_3h MC_endcaps_figs_3h_OLD
 root -l -b -q comp_3h.C++\(\"BsMC12_endcaps_preselection_0.root\",\"BsMC12_endcaps_preselection_1.root\",\"BsMC12_endcaps_preselection_2.root\"\)
 mv figs_3h MC_endcaps_figs_3h
+cp MC_endcaps_figs_3h/*.pdf MC_endcaps_figs_3h/*/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/MC_endcaps_figs_3h/
 cat page_template.php | sed s/"TITLE"/"MC Endcaps"/g > MC_endcaps_figs_3h/page.php
