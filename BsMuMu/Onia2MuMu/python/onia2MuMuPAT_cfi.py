98fc8e0a2427ef2c41a9ac217a1ead591a0855a4
fiori <>
2013-04-16 15:01:38 +0000

    new cfi for normalization channel added

--- BsMuMu/Onia2MuMu/python/onia2MuMuPAT_cfi.py
+++ BsMuMu/Onia2MuMu/python/onia2MuMuPAT_cfi.py
@@ -12,5 +12,12 @@ onia2MuMuPAT = cms.EDProducer('Onia2MuMuPAT',
   addMCTruth = cms.bool(True),      ## Add the common MC mother of the two muons, if any
   resolvePileUpAmbiguity = cms.bool(True),   ## Order PVs by their vicinity to the J/psi vertex, not by sumPt
   # Apply any preselection cut. These cuts are applied just before the candidates are saved in the collection, at the end of the onia2MuMuPAT.
+  addThirdTrack = cms.bool(True),  ## search a third track making a good vertex with the dimuon
+  minTrackPt =cms.double(0.5), ## minimum pt of the third track
+  trackMass = cms.double(0.4936), ## mass for the track
+  diMuMassMin= cms.double(2.8),
+  diMuMassMax= cms.double(3.4),
+  diMuPlusTrackMassMin = cms.double(4.9),
+  diMuPlusTrackMassMax = cms.double(5.9),
   preselection = cms.string("")
 )
