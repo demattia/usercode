c5f10a52df1ec1a93f3b4a6d605845abc65b05ef
nuno <>
2013-05-01 00:47:01 +0000

    add other mva methods

--- BsMuMu/Macros/runTMVA.sh
+++ BsMuMu/Macros/runTMVA.sh
@@ -1,12 +1,12 @@
 rm TMVALog_*.txt 
 root -b -q -l compileTMVA.C
-root -b -q -l TMVAClassification.C+\(\"barrel\",\"\",\"\"\) > TMVALog_barrel.txt &
-root -b -q -l TMVAClassification.C+\(\"endcaps\",\"\",\"\"\) > TMVALog_endcaps.txt &
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"\",\"BDT,MLP,CutsSA\"\) > TMVALog_barrel.txt &
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"\",\"BDT,MLP,CutsSA\"\) > TMVALog_endcaps.txt &
 
-root -b -q -l TMVAClassification.C+\(\"barrel\",\"0\",\"\"\) > TMVALog_barrel_0.txt &
-root -b -q -l TMVAClassification.C+\(\"barrel\",\"1\",\"\"\) > TMVALog_barrel_1.txt &
-root -b -q -l TMVAClassification.C+\(\"barrel\",\"2\",\"\"\) > TMVALog_barrel_2.txt &
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"0\",\"BDT,MLP,CutsSA\"\) > TMVALog_barrel_0.txt &
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"1\",\"BDT,MLP,CutsSA\"\) > TMVALog_barrel_1.txt &
+root -b -q -l TMVAClassification.C+\(\"barrel\",\"2\",\"BDT,MLP,CutsSA\"\) > TMVALog_barrel_2.txt &
 
-root -b -q -l TMVAClassification.C+\(\"endcaps\",\"0\",\"\"\) > TMVALog_endcaps_0.txt &
-root -b -q -l TMVAClassification.C+\(\"endcaps\",\"1\",\"\"\) > TMVALog_endcaps_1.txt &
-root -b -q -l TMVAClassification.C+\(\"endcaps\",\"2\",\"\"\) > TMVALog_endcaps_2.txt &
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"0\",\"BDT,MLP,CutsSA\"\) > TMVALog_endcaps_0.txt &
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"1\",\"BDT,MLP,CutsSA\"\) > TMVALog_endcaps_1.txt &
+root -b -q -l TMVAClassification.C+\(\"endcaps\",\"2\",\"BDT,MLP,CutsSA\"\) > TMVALog_endcaps_2.txt &
