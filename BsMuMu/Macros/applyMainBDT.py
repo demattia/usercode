6a591db3cf4f8f659294efe7cb617070f976717d
nuno <>
2013-07-04 20:41:13 +0000

    fixed cnc, added functionality

--- BsMuMu/Macros/applyMainBDT.py
+++ BsMuMu/Macros/applyMainBDT.py
@@ -12,16 +12,18 @@ method="BDT"
 Region = ""
 cutValue = -999
 
-runOnMainData = False
+runOnMainData = True
 
 tree_name = "probe_tree"
 if runOnMainData: tree_name = "events"
-    
+
 with open("setdirs.h", "a") as myfile:
     if runOnMainData: 
         myfile.write("typedef double mytype;")
     else:
         myfile.write("typedef float mytype;")
+
+#exit(44)
 
 strname_ = ""
 if runOnMainData:
@@ -35,12 +37,18 @@ def main():
     outputfilelistB = ""
     outputfilelistE= ""
 
-    mainBdtCut = { "barrel":0.350, "endcaps":0.368 }
+    mainBdtCut = { "barrel":0.360, "endcaps":0.368 }
     #mainBdtCut = { "barrel":0, "endcaps":0 }
+
+    #rootfiles/all_mainBDTonMainData_tree.root 
+
+    #sequential_count("barrel")
+    #exit(77)
 
     #main ana BDT application
     for isample in range(3):
         for ireg,region in enumerate(regions):
+            #if region == "endcaps" or isample>0 : continue
             Region = region[:1].upper()+region[1:] #capitalize first letter for retrieving file names
             cutValue = mainBdtCut[region]
 
@@ -60,27 +68,27 @@ def main():
             print isample, Region, mainWeightFile, inputFile, outputFile
             os.system("root -l -b -q TMVAClassificationApplication_main.C+\(\\\""+inputFile+"\\\",\\\""+outputFile+"\\\",\\\""+mainWeightFile+"\\\","+str(cutValue)+",\\\""+method+"\\\",\\\""+region+"\\\",\\\""+tree_name+"\\\"\)")
 
-#    #merge files
-#    #print "hadd rootfiles/all_mainBDTapplied.root",outputfilelist
-#    os.system("hadd -f "+rootDir+"all"+strname_+outputfilelist)
-#    os.system("hadd -f "+rootDir+"barrel"+strname_+outputfilelistB)
-#    os.system("hadd -f "+rootDir+"endcaps"+strname_+outputfilelistE)
-#
-#    arr = ["a","b.root","c.root"]
-#    arra = []
-#    for i,a in enumerate(arr):
-#        b=a
-#        if i: b = a.split(".")[0] + "_tree." + a.split(".")[1]
-#        arra.append(b)
-#        print arra
+#    exit(55)
 
+    cmd = "hadd -f "+rootDir+"all"+strname_+outputfilelist
+    os.system(cmd)
+    #cmd=cmd.replace(".root","_tree.root")
+    os.system(cmd)
+    #print cmd
+    #exit(66)
+    cmd = "hadd -f "+rootDir+"barrel"+strname_+outputfilelistB
+    os.system(cmd)
+    #os.system(cmd.replace(".root","_tree.root"))
+    cmd = "hadd -f "+rootDir+"endcaps"+strname_+outputfilelistE
+    os.system(cmd)
+    #os.system(cmd.replace(".root","_tree.root"))
+
+    exit(66)
 
 
     # display plots
     for region in regions:
         TMVAPlots(region)
-
-    exit(66)
 
     #display mass plot and event count after each Cnc cut
     for region in regions:
@@ -89,27 +97,47 @@ def main():
 
 
 #list of main analysis CnC cuts
-listOfCntCutsBarrel = ["",
+listOfCntCutsBarrel = ["","mu1_GM && mu2_GM",
                  "((m1pt>m2pt && m1pt>4.5 && m2pt>4.0)||(m1pt<m2pt && m1pt>4.0 && m2pt>4.5))",
                  "pt>6.5", "pvw8>0.6","pvip<0.008", "pvips<2",  
-                 "alpha<0.05", "chi2dof<2.2", "fls3d>13", "iso>0.8", "maxdoca>0.015", "closetrk<2" 
+                 "alpha<0.05", "chi2dof<2.2", "fls3d>13", "iso>0.8", "docatrk>0.015", "closetrk<2" 
                  ]
-listOfCntCutsEndcaps = ["",
+listOfCntCutsEndcaps = ["","mu1_GM && mu2_GM",
                        "((m1pt>m2pt && m1pt>4.5 && m2pt>4.2)||(m1pt<m2pt && m1pt>4.2 && m2pt>4.5))",
                  "pt>8.5", "pvw8>0.6","pvip<0.008", "pvips<2",  
-                 "alpha<0.03", "chi2dof<1.8", "fls3d>15", "iso>0.8", "maxdoca>0.015", "closetrk<2" 
+                 "alpha<0.03", "chi2dof<1.8", "fls3d>15", "iso>0.8", "docatrk>0.015", "closetrk<2" 
                  ]
+
+#barrel in root format:                 "((m1pt>m2pt && m1pt>4.5 && m2pt>4.0)||(m1pt<m2pt && m1pt>4.0 && m2pt>4.5))&&pt>6.5&&pvw8>0.6&&pvip<0.008&&pvips<2&&alpha<0.05&&chi2dof<2.2&&fls3d>13&&iso>0.8&&docatrk>0.015&&closetrk<2" 
+# events->Draw("m", "m1pt>4.5 && m2pt>4.0&&pt>6.5&&pvw8>0.6&&pvip<0.008&&pvips<2&&alpha<0.05&&chi2dof<2.2&&fls3d>13&&iso>0.8&&docatrk>0.015&&closetrk<2")
+
+
 
 Ncuts = len(listOfCntCutsBarrel)
     
 gStyle.SetOptStat(0)
 
+
+#def sequential_count(region):
+#    Region = region[:1].upper()+region[1:] #capitalize first letter for retrieving file names
+#    fname = rootDir + Region+"_preselection.root"
+#    print fname
+#    inputFile = TFile(rootDir + Region+"_preselection.root")
+#    inputDir = inputFile.Get("probe_tree")
+
 def sequential_count(region):
     Region = region[:1].upper()+region[1:] #capitalize first letter for retrieving file names
     fname = rootDir + Region+"_preselection.root"
-    print fname
-    inputFile = TFile(rootDir + Region+"_preselection.root")
-    inputDir = inputFile.Get("probe_tree")
+    ireg = 1
+    if(region == "barrel"):
+            ireg = 0
+    if runOnMainData:
+        fname = "trees_main/data_afterCuts_"+str(ireg)+".root"
+    print fname, tree_name
+    inputFile = TFile(fname)
+    inputDir = inputFile.Get(tree_name)
+    #inputDir.ls()
+
     cuts = ""
     icol=0
     massPlot = []
@@ -162,9 +190,14 @@ def sequential_count(region):
             tl.DrawLatex(0.38,yy,"")
             yy-=step
 
-    canvas.SaveAs(plotsDir+"mass_cnt_"+region+".gif")
-    canvas.SaveAs(figuresDird+"mass_cnt_"+region+".pdf")
-
+    fname  = "mass_cuts_cnt_" + region + strname_.split(".")[0]
+    canvas.SaveAs(plotsDir+fname+".gif")
+    canvas.SaveAs(figuresDird+fname+".pdf")
+    canvass = TCanvas()
+    massPlot[Ncuts-1].Draw()
+    fname  = "mass_cnt_" + region + strname_.split(".")[0]
+    canvass.SaveAs(plotsDir+fname+".gif")
+    canvass.SaveAs(figuresDird+fname+".pdf")
 
 
 def TMVAPlots(region):
