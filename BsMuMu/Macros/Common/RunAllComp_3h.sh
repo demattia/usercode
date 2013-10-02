5ac0fa614824e51c6edb8f3edf65fa94a06e184e
Zhen Hu <Zhen.Hu@cern.ch>
2013-10-02 11:31:07 +0200

    fixbug

--- BsMuMu/Macros/Common/RunAllComp_3h.sh
+++ BsMuMu/Macros/Common/RunAllComp_3h.sh
@@ -21,7 +21,7 @@ rm -rf Data_endcaps_figs_3h_OLD
 mv Data_endcaps_figs_3h Data_endcaps_figs_3h_OLD
 ${rootExecutable} -l -b -q comp_3h.C++\(\"../rootfiles/Endcaps_preselection_0.root\",\"../rootfiles/Endcaps_preselection_1.root\",\"../rootfiles/Endcaps_preselection_2.root\"\)
 mv figs_3h Data_endcaps_figs_3h
-cp -rf Data_endcaps_figs_3h/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/
+cp -rf Data_endcaps_figs_3h ../BsMuMuLatex/Figures/VariablesComparison/
 cat page_template.php | sed s/"TITLE"/"Data Endcaps"/g > Data_endcaps_figs_3h/page.php
 
 # MC
@@ -31,7 +31,7 @@ rm -rf MC_barrel_figs_3h_OLD
 mv MC_barrel_figs_3h MC_barrel_figs_3h_OLD
 ${rootExecutable} -l -b -q comp_3h.C++\(\"../rootfiles/BsMC12_barrel_preselection_0.root\",\"../rootfiles/BsMC12_barrel_preselection_1.root\",\"../rootfiles/BsMC12_barrel_preselection_2.root\"\)
 mv figs_3h MC_barrel_figs_3h
-cp -rf MC_barrel_figs_3h/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/
+cp -rf MC_barrel_figs_3h ../BsMuMuLatex/Figures/VariablesComparison/
 cat page_template.php | sed s/"TITLE"/"MC Barrel"/g > MC_barrel_figs_3h/page.php
 
 mkdir figs_3h
@@ -39,7 +39,7 @@ rm -rf MC_endcaps_figs_3h_OLD
 mv MC_endcaps_figs_3h MC_endcaps_figs_3h_OLD
 ${rootExecutable} -l -b -q comp_3h.C++\(\"../rootfiles/BsMC12_endcaps_preselection_0.root\",\"../rootfiles/BsMC12_endcaps_preselection_1.root\",\"../rootfiles/BsMC12_endcaps_preselection_2.root\"\)
 mv figs_3h MC_endcaps_figs_3h
-cp -rf MC_endcaps_figs_3h/*.pdf ../BsMuMuLatex/Figures/VariablesComparison/
+cp -rf MC_endcaps_figs_3h ../BsMuMuLatex/Figures/VariablesComparison/
 cat page_template.php | sed s/"TITLE"/"MC Endcaps"/g > MC_endcaps_figs_3h/page.php
 
 cd -
