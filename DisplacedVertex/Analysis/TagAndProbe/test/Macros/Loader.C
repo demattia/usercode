e72473ecdb16f9d02bc500781f8403b220195dc9
demattia <marco.de.mattia@cern.ch>
2013-10-22 19:52:50 +0200

    For the objects

---
+++ DisplacedVertex/Analysis/TagAndProbe/test/Macros/Loader.C
@@ -0,0 +1,12 @@
+#include <vector>
+#include <map>
+#include <string>
+#include "Analysis/RootTreeProducers/interface/Track.h"
+#include "Analysis/RootTreeProducers/interface/GenParticle.h"
+#ifdef __CINT__
+#pragma link C++ class Track+;
+#pragma link C++ class std::vector<Track>+;
+#pragma link C++ class GenParticle+;
+#pragma link C++ class std::vector<GenParticle>+;
+#pragma link C++ class std::map<std::string,std::vector<Track> >+;
+#endif
